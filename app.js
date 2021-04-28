a0aa53ad5b5964dc30196f9494c95697de096079
JonathanGab <jogaborit@hotmail.com>
2021-04-28 17:42:55 +0200

    add  response api sendStatus500

--- app.js
+++ app.js
@@ -168,11 +168,13 @@ app.post('/contact', (req, res) => {
   transporter.sendMail(message, (err, info) => {
     if (err) {
       console.log('Error occurred. ' + err.message);
-      return process.exit(1);
+
+      res.sendStatus(500);
+    } else {
+      console.log('Message sent: %s', info.messageId);
+      res.sendStatus(200);
     }
 
-    console.log('Message sent: %s', info.messageId);
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
   });
 });
