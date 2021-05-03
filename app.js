998195e08093655160b3b35e846788dbcd80f665
Edouard Misset <edouardmisset@gmail.com>
2021-05-03 18:44:07 +0200

    fixed post route issue regarding user_name

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { PORT, CORS_ALLOWED_ORIGINS, inTestEnv } = require('./env');
 const app = express();
 app.use(express.json());
 
-// Connectino error
+// Connection error
 connection.connect((err) =>
   err
     ? console.error(`error connecting: ${err.stack}`)
@@ -72,13 +72,13 @@ app.get('/movies/:tmdb_id/reviews', (req, res) => {
 
 // Add a review to a movie
 app.post('/movies/:tmdb_id/reviews', (req, res) => {
-  const { title, userName, comment } = req.body;
+  const { title, user_name, comment } = req.body;
   const { tmdb_id } = req.params;
   connection
     .promise()
     .query(
       'INSERT INTO reviews (title, tmdb_id, comment, user_name) VALUES (?, ?, ?, ?)',
-      [title, tmdb_id, comment, userName]
+      [title, tmdb_id, comment, user_name]
     )
     .then(([results]) => {
       const newComment = {
@@ -86,7 +86,7 @@ app.post('/movies/:tmdb_id/reviews', (req, res) => {
         title,
         tmdb_id,
         comment,
-        userName,
+        user_name,
       };
       res.send(newComment);
     })
